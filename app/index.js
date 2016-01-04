9ecee0280ef01ab8752866e116d673d5c16649e3
kostasmav <kostasmav4@gmail.com>
2016-01-04 05:27:01 +0200

    Window position saved on close

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 // Module to control application life.
 var app = require('app');
 
+var path = require('path');
+var fs = require('fs');
+var initPath = path.join(app.getPath('userData'), 'position.json');
+var dataFilePath = path.join(app.getPath('userData'), 'passwords.json');
+
 // Module to create native browser window.
 var BrowserWindow = require('browser-window');
 
@@ -8,25 +13,30 @@ var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
+  if (process.platform != 'darwin') {    
     app.quit();
   }
 });
 
 
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function () {
+  
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1024, height: 800 });
+  // If there is a file position.json get position and create mainWindow
+  try {
+      var data = JSON.parse(fs.readFileSync(initPath));
+      mainWindow = new BrowserWindow({ width: data.width, height: data.height, x: data.x, y: data.y });
+  // Else create a default mainWindow    
+  } catch (err) {
+      console.error(err);
+      mainWindow = new BrowserWindow({ width: 1024, height: 800 });
+  }
 
-  // Get userData path to save passwords.json
-  var path = require('path');
-  var fs = require('fs');
-
-  var dataFilePath = path.join(app.getPath('userData'), 'passwords.json');
+  // Get userData path to save passwords.json  
   mainWindow.dataFilePath = dataFilePath;
-  console.log(dataFilePath);
 
   // Check if the file exists
   fs.access(dataFilePath, fs.F_OK, function(err) {
@@ -38,8 +48,7 @@ app.on('ready', function () {
               return console.log(err);
           } else {
             console.log("Passwords.json file was created!");
-          }
-          
+          }          
       });
     }
   }); // Ends - Get userData path to save passwords.json -
@@ -49,6 +58,13 @@ app.on('ready', function () {
 
   // Open the devtools.
   // mainWindow.openDevTools();
+
+  // Emitted before the window is closed
+  mainWindow.on('close', function () {       
+      // Get window position and write it in userData/position.json    
+      var windowPosition = mainWindow.getBounds();
+      fs.writeFileSync(initPath, JSON.stringify(windowPosition));
+  });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
