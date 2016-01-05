41b91bc925a2c5e953f35f3dfd9ee5ee55bf0d2d
kostasmav <kostasmav4@gmail.com>
2016-01-06 00:41:57 +0200

    Added check for duplicates on password creation

--- app/scripts/create-password/create-password.js
+++ app/scripts/create-password/create-password.js
@@ -15,6 +15,7 @@ angular.module('myPasswords.create-password', ['ngRoute'])
 	var remote = require('remote');
 	var userDataPath = remote.getCurrentWindow().dataFilePath;
 	var fs = require('fs');
+	var counter = 0;
 
     $scope.create = function(password) {
     	$scope.success = false;
@@ -26,7 +27,7 @@ angular.module('myPasswords.create-password', ['ngRoute'])
 			var passwordObjects = [];
 			// If file is empty			
 		  	if (data === '') {
-		  		passwordObjects.push(passwordData);
+		  		passwordObjects.push(passwordData);		  		
 
 		  		// Write into the file
 		  		fs.writeFile(userDataPath, JSON.stringify(passwordObjects), function (err) {
@@ -34,32 +35,51 @@ angular.module('myPasswords.create-password', ['ngRoute'])
 				        return console.log(err);
 				    } else {
 					    console.log("The file was saved!");
-					    $scope.success = true;
-					    // Set success to false after 5 secs
+					    $scope.successMessage = 'Password created!';
+					    // Set successMessage to false after 5 secs
 					    $timeout(function(){
-							$scope.success = false;
-				       	}, 500);
+							$scope.successMessage = '';
+				       	}, 2000);
 
 					}
 				}); 
 			// If file is not empty
 		  	} else {
-		  		passwordObjects = JSON.parse(data);
-		  		passwordObjects.push(passwordData);
+		  		// Get password objects
+		  		passwordObjects = JSON.parse(data);		  		
 
-		  		// Write into the file
-				fs.writeFile(userDataPath, JSON.stringify(passwordObjects), function (err) {
-					if (err) {
-				  		return console.log(err);
-					} else {
-					  	console.log("The file was saved!");
-					  	$scope.success = true;
-					  	// Set success to false after 5 secs
-					  	$timeout(function(){
-							$scope.success = false;
-				       	}, 5000);
-				  	}
-				});
+		  		// Check if password exists
+			  	for(var j = 0; j < passwordObjects.length; j++) {
+			  		if(passwordObjects[j].name === passwordData.name) {	
+			  		counter += 1;
+			  		}		  		
+			  	}
+			  	// If found at least 1 then its a duplicate
+			  	if (counter > 0) {
+			  		$scope.errorMessage = 'Password name already exists!';
+			  		$timeout(function(){
+				    	$scope.errorMessage = '';
+						$scope.successMessage = '';
+						counter = 0;
+			       	}, 2000);
+			  	} else {
+			  		// Else push the new password in
+			  		passwordObjects.push(passwordData);
+
+			  		// Write into the file
+					fs.writeFile(userDataPath, JSON.stringify(passwordObjects), function (err) {
+						if (err) {
+					  		return console.log(err);
+						} else {
+						  	console.log("The file was saved!");
+						  	$scope.successMessage = 'Password created!';
+						  	// Set success to false after 5 secs
+						  	$timeout(function(){
+								$scope.successMessage = '';
+					       	}, 2000);
+					  	}
+					});
+			  	}
 		  	}		  	
 		});
 
